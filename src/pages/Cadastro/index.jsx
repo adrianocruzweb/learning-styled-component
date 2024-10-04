b02b8e440914757aa77a9e4ae6a8a40397429ef0
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-10-04 18:25:49 -0300

    add cadastro page container

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,6 +1,6 @@
 import { Header } from "../../components/Header";
 import {
-
+  Container
 } from "./styles";
 
 const Cadastro = () => {
@@ -10,7 +10,10 @@ const Cadastro = () => {
 
   return (<>
       <Header>Home</Header>
-
+      <Container>
+        <div>A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas.</div>
+        <div>form</div>
+      </Container>
     </>);
 }
 
--- src/pages/Cadastro/styles.js
+++ src/pages/Cadastro/styles.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    width: 100%;
+    max-width: 80%;
+    height: 47px;
 
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
 `
